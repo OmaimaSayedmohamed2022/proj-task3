e9d276c86295a387e819374417dff485309bed3b
OmaimaSayed2022 <115481386+OmaimaSayed2022@users.noreply.github.com>
2023-07-10 14:37:18 +0200

    proj-task3

---
+++ routers/user.js
@@ -0,0 +1,18 @@
+
+const express = require ('express')
+const User = require('../models/user')
+
+
+const router = express.Router()
+
+router.post ('/users' , (req , res) => {
+    console.log(req.body)
+
+    const user = new User (req.body)
+
+    user.save()
+    .then ((user) => {res.status(200).send(user)})
+    .catch((e)=>{ res.status(400).send(e)})
+})
+
+module.exports = router 
